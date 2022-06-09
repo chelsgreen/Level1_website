3522019ee98b3a5d34eba73a48ff9acbb1857116
chelsgreen <99847192+chelsgreen@users.noreply.github.com>
2022-06-09 12:35:58 +1200

    Project Setup
    -Create folder structure
    -Add basic structure tags

--- index.php
+++ index.php
@@ -5,17 +5,7 @@
     </head>
 
     <body>
-        <div class="wrapper">
-            <div class="logo box">    
-            </div>
-            <div class="banner box">Welcome to Guitars</div>
-    
-            <div class="nav box">
-                <a href="index.php"> Home |</a>
-                <a href=".php"> Learn |</a>
-                <a href=".php"> Videos|</a>
-                <a href=".php"> Contacts|</a>
-            </div>
+       
        
            
     </body>
