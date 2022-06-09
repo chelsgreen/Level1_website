fdd1a7454dd4e0611a93c3c0dc09d44d9bc1747d
chelsgreen <99847192+chelsgreen@users.noreply.github.com>
2022-06-09 12:30:08 +1200

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head> 
+        <!---Link to style sheet-->
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <div class="wrapper">
+            <div class="logo box">    
+            </div>
+            <div class="banner box">Welcome to Guitars</div>
+    
+            <div class="nav box">
+                <a href="index.php"> Home |</a>
+                <a href=".php"> Learn |</a>
+                <a href=".php"> Videos|</a>
+                <a href=".php"> Contacts|</a>
+            </div>
+       
+           
+    </body>
+</html>    
